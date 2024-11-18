46d196e937138c53e2fc55ad88d342b9aa7514e3
nastaran akbarian <nastaranakbarian81@gmail.com>
2024-11-18 22:25:23 +0330

    changed

--- 01.py
+++ 01.py
@@ -5,7 +5,9 @@ initial_capital=1000
 capital=initial_capital
 
 
-data = yf.download("BTC-USD", start="2010-07-19", interval="1d")
+data = yf.download("BTC-USD", start="2015-01-01",end="2022-12-31", interval="1d")
+
+
 data ['weekday']= data.index.weekday
 
 mondays = data[data['weekday']==0]
@@ -22,13 +24,13 @@ for index, row in transactions.iterrows():
     btc_bought = capital / row['Open_buy']
     capital = btc_bought * row['Close_sell']
 
-    profit_or_loss = capital - initial_capital
+profit_or_loss = capital - initial_capital
 
-    print(f"sarmaye nahaii: {capital:.2f} dolar ")
-    if profit_or_loss > 0:
-        print(f"shoma sood kardin :{profit_or_loss:.2f} dolar ")
-    else:
-        print(f"shoma zarar kardin :{profit_or_loss:.2f} dolar ")
+print(f"sarmaye nahaii: {capital:.2f} dolar ")
+if profit_or_loss > 0:
+    print(f"shoma sood kardin: {profit_or_loss:.2f} dolar ")
+else:
+    print(f"shoma zarar kardin :{profit_or_loss:.2f} dolar ")
 
 # print(data.head())
 
